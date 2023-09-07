5c5e3c8a8814b27f0e4e24bc3682012b0e99f62b
GustavoMiranda01 <gustavomiranda.contato@gmail.com>
2023-09-07 08:12:18 -0300

    justes price

--- assets/js/dados.js
+++ assets/js/dados.js
@@ -5,84 +5,84 @@ var MENU = {
             "img": "./assets/img/cardapio/churrasco/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg",
             "name": "Joe's KC BBQ",
             "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
-            "price": 110.99
+            "price": 37.99
         },
         {
             "id": "005-kings-carolina-oink-sampler",
             "img": "./assets/img/cardapio/churrasco/carolina-bbq-oink-sampler.1340b5a10cedc238cb2280306dd1d5a5.jpg",
             "name": "Kings BBQ",
             "dsc": "Carolina BBQ Oink Sampler",
-            "price": 89
+            "price": 42
         },
         {
             "id": "texas-monthlys-1-bbq-brisket",
             "img": "./assets/img/cardapio/churrasco/texas-monthlys-1-bbq-brisket.1006a061be7acae03992e420fbca995a.jpg",
             "name": "Snow's BBQ",
             "dsc": "Texas Monthly's #1 BBQ Brisket",
-            "price": 199
+            "price": 39
         },
         {
             "id": "whole-brisket-texas-bbq-sauce",
             "img": "./assets/img/cardapio/churrasco/whole-brisket-texas-barbecue-bbq-sauce.e07ee4818b20ed43d217bf67fedd41ce.jpg",
             "name": "Franklin Barbecue",
             "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
-            "price": 249
+            "price": 49
         },
         {
             "id": "whole-texas-smoked-brisket",
             "img": "./assets/img/cardapio/churrasco/whole-texas-smoked-brisket.a5558a25381e271408e197936e7985d8.jpg",
             "name": "Terry Black's Barbecue",
             "dsc": "Whole Texas Smoked Brisket",
-            "price": 189
+            "price": 39
         },
         {
             "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
             "img": "./assets/img/cardapio/churrasco/mini-trinity-bbq-combo-brisket-ribs-and-links.245582f593bf64b23b57dfca2be18cfd.jpg",
             "name": "Bludso's BBQ",
             "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
-            "price": 139
+            "price": 39
         },
         {
             "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
             "img": "./assets/img/cardapio/churrasco/blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo.a95a3e632ae324f719738a2a5c1dff6e.jpg",
             "name": "Blue Smoke",
             "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
-            "price": 129
+            "price": 29
         },
         {
             "id": "006-kings-meat-lovers-special",
             "img": "./assets/img/cardapio/churrasco/bbq-meat-lovers-special-for-10.36ca670fda4bfa783c2ea9165e068d26.jpg",
             "name": "Kings BBQ",
             "dsc": "BBQ Meat Lovers Special for 10",
-            "price": 139
+            "price": 39
         },
         {
             "id": "the-big-ugly-bbq-dinner-for-6",
             "img": "./assets/img/cardapio/churrasco/the-big-ugly-bbq-dinner-for-6.2dfae7818811adddce85cc1a910881a0.jpg",
             "name": "Ugly Drum",
             "dsc": "The Big Ugly BBQ Dinner for 6",
-            "price": 229
+            "price": 45
         },
         {
             "id": "17796-mighty-quinns-bbq-sampler-pack",
             "img": "./assets/img/cardapio/churrasco/mighty-quinns-bbq-sampler-pack.1bfe4a0665edc565756f5241bf25840e.jpg",
             "name": "Mighty Quinn's BBQ",
             "dsc": "Mighty Quinn's BBQ Sampler Pack",
-            "price": 169
+            "price": 43
         },
         {
             "id": "post-oak-smoked-half-brisket",
             "img": "./assets/img/cardapio/churrasco/post-oak-smoked-usda-prime-half-brisket.6f17178dde7d806670bcc73ff11762b3.jpg",
             "name": "Southside Market & Barbeque",
             "dsc": "Post Oak Smoked USDA Prime Half Brisket",
-            "price": 109
+            "price": 28
         },
         {
             "id": "best-of-texas-bbq-combo-serves-14",
             "img": "./assets/img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
             "name": "Snow's BBQ",
             "dsc": "Best of Texas BBQ Combo - Serves 14",
-            "price": 269
+            "price": 25
         }
     ],
     "burgers": [
@@ -91,84 +91,84 @@ var MENU = {
             "img": "./assets/img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
             "name": "Gramercy Tavern",
             "dsc": "The Gramercy Tavern Burger - 4 Pack",
-            "price": 99
+            "price": 43
         },
         {
             "id": "shake-shack-shackburger-8-pack",
             "img": "./assets/img/cardapio/burguers/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.jpg",
             "name": "Shake Shack",
             "dsc": "Shake Shack ShackBurger® - 8 Pack",
-            "price": 49
+            "price": 40
         },
         {
             "id": "gotts-cheeseburger-kit-for-4",
             "img": "./assets/img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg",
             "name": "Gott's Roadside",
             "dsc": "Gott's Complete Cheeseburger Kit for 4",
-            "price": 99
+            "price": 38
         },
         {
             "id": "le-big-matt-kit-for-6",
             "img": "./assets/img/cardapio/burguers/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg",
             "name": "Emmy Squared",
             "dsc": "Le Big Matt Burger Kit for 6",
-            "price": 99
+            "price": 35
         },
         {
             "id": "shake-shack-shackburger-16-pack",
             "img": "./assets/img/cardapio/burguers/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.jpg",
             "name": "Shake Shack",
             "dsc": "Shake Shack Shackburger® - 16 Pack",
-            "price": 89
+            "price": 35
         },
         {
             "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
             "img": "./assets/img/cardapio/burguers/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.jpg",
             "name": "Peter Luger Steak House",
             "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
-            "price": 175.95
+            "price": 49.90
         },
         {
             "id": "double-stack-burger-kit-for-4",
             "img": "./assets/img/cardapio/burguers/handf-double-stack-burger-kit-for-4.4ee9f54b1d6087e9996335f07c13e5cd.jpg",
             "name": "Holeman & Finch",
             "dsc": "Double Stack Burger Kit for 4",
-            "price": 79
+            "price": 39.90
         },
         {
             "id": "goldbelly-burger-bash-pack",
             "img": "./assets/img/cardapio/burguers/the-burger-bash-package.bd9d12d031865940bbe5faf15f1a62f8.jpg",
             "name": "Pat LaFrieda Meats",
             "dsc": "Goldbelly \"Burger Bash\" Pack",
-            "price": 109
+            "price": 35.90
         },
         {
             "id": "burger-au-poivre-kit-4-pack",
             "img": "./assets/img/cardapio/burguers/burger-au-poivre-kit-4-pack.3ca0e39b02db753304cd185638dad518.jpg",
             "name": "Raoul's",
             "dsc": "Burger Au Poivre Kit - 4 Pack",
-            "price": 99
+            "price": 45.90
         },
         {
             "id": "goldbelly-burger-blend-4-lbs",
             "img": "./assets/img/cardapio/burguers/goldbelly-burger-blend-1-lb.13a21b66edf7173a59c75c3a6d2f981b.jpg",
             "name": "Flannery Beef",
             "dsc": "Goldbelly Burger Blend - 4 lbs.",
-            "price": 79
+            "price": 49.90
         },
         {
             "id": "gotts-complete-cheeseburger-kit-for-8",
             "img": "./assets/img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-8.092aa049d00286fa1733d720decc782e.jpg",
             "name": "Gott's Roadside",
             "dsc": "Gott's Complete Cheeseburger Kit for 8",
-            "price": 149
+            "price": 38.90
         },
         {
             "id": "gramercy-tavern-burger-kielbasa-combo",
             "img": "./assets/img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-47.jpg",
             "name": "Gramercy Tavern",
             "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
-            "price": 149
+            "price": 49.90
         }
     ],
     "sobremesas": [
@@ -177,84 +177,84 @@ var MENU = {
             "img": "./assets/img/cardapio/sobremesas/german-chocolate-killer-brownie-tin-pack.5ebc34160f28767a9d94c4da2e04c4b9.jpg",
             "name": "Killer Brownie®",
             "dsc": "German Chocolate Killer Brownie®",
-            "price": 39.99
+            "price": 28.90
         },
         {
             "id": "jacques-world-famous-chocolate-chip-cookies",
             "img": "./assets/img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-6-pack.2217a14c443602493bba88aa9335319a.jpg",
             "name": "Jacques Torres Chocolate",
             "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
-            "price": 39.95
+            "price": 28.90
         },
         {
             "id": "luigis-original-cannoli-pie",
             "img": "./assets/img/cardapio/sobremesas/original-cannoli-pie.4cb5b9ba82f57b69b90765fd9f07aa1a.jpg",
             "name": "The Cannoli Pie Company",
             "dsc": "Original Cannoli Pie",
-            "price": 69
+            "price": 28.90
         },
         {
             "id": "sea-salted-caramel-swirl-cheesecake",
             "img": "./assets/img/cardapio/sobremesas/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg",
             "name": "Cotton Blues Cheesecake Company",
             "dsc": "Sea-Salted Caramel Swirl Cheesecake",
-            "price": 65
+            "price": 27
         },
         {
             "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
             "img": "./assets/img/cardapio/sobremesas/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a7.jpg",
             "name": "Brooklyn Blackout Company",
             "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
-            "price": 89
+            "price": 28.90
         },
         {
             "id": "best-seller-cupcake-dozen",
             "img": "./assets/img/cardapio/sobremesas/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08f.jpg",
             "name": "Crave Cupcakes",
             "dsc": "Best Seller Cupcake Dozen",
-            "price": 89
+            "price": 28.90
         },
         {
             "id": "choose-your-own-ice-cream-donuts-6-pack",
             "img": "./assets/img/cardapio/sobremesas/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
             "name": "Elegant Desserts",
             "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
-            "price": 69
+            "price": 28.90
         },
         {
             "id": "17481-jewish-dessert-3-pack",
             "img": "./assets/img/cardapio/sobremesas/jewish-classics-dessert-pack.7d4b76630f2fe75dcb7bbcf2501b7390.jpg",
             "name": "Ess-a-Bagel",
             "dsc": "Jewish Classics Dessert Pack",
-            "price": 89.95
+            "price": 28.90
         },
         {
             "id": "dessert-bar-care-package",
             "img": "./assets/img/cardapio/sobremesas/dessert-bar-care-package.324aa28fe98c9dc67b75aac43376902e.jpg",
             "name": "Bread and Roses Bakery",
             "dsc": "Dessert Bar Care Package",
-            "price": 65
+            "price": 28.90
         },
         {
             "id": "donut-cookies-12-pack",
             "img": "./assets/img/cardapio/sobremesas/donut-cookies-12-pack.46f42c61c4a79fd2051a99b2f23e890e.jpg",
             "name": "Stan's Donuts",
             "dsc": "Donut Cookies - 12 Pack",
-            "price": 49
+            "price": 28.90
         },
         {
             "id": "gulab-jamun-ice-cream-cakes-2-pack",
             "img": "./assets/img/cardapio/sobremesas/gulab-jamun-ice-cream-cakes-2-pack.c45b4d0750ad22d741f84dc1f26d20e7.jpg",
             "name": "Malai Ice Cream",
             "dsc": "Gulab Jamun Ice Cream Cakes - 2 Pack",
-            "price": 79
+            "price": 28.90
         },
         {
             "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
             "img": "./assets/img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-12-pack.3b373bdd67cd25084182c21499f675d1.jpg",
             "name": "Jacques Torres Chocolate",
             "dsc": "Jacques' World Famous Chocolate Chip Cookies - 12 Pack",
-            "price": 69.95
+            "price": 28.90
         }
     ],
     "bebidas": [
@@ -305,14 +305,14 @@ var MENU = {
             "img": "./assets/img/cardapio/bebidas/unicorn-parade-milkshake-kit-for-2.9052d04c1cf25b29442048bd3e535f21.jpg",
             "name": "New Territories",
             "dsc": "Unicorn Parade Milkshake Kit for 8",
-            "price": 109
+            "price": 29
         },
         {
             "id": "chickpea-chiller-kit-for-6",
             "img": "./assets/img/cardapio/bebidas/chickpea-chiller-kit-for-6.4310765c71ba524b5462ea9330d32446.jpg",
             "name": "The Hummus & Pita Co.",
             "dsc": "Chickpea Chiller Kit for 6",
-            "price": 89
+            "price": 25
         },
         {
             "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
@@ -326,7 +326,7 @@ var MENU = {
             "img": "./assets/img/cardapio/bebidas/kentucky-derby-mint-julep-gift-set.79720eda4e9c8e3fcf9ecb5c79827f2c.jpg",
             "name": "Woodford Reserve",
             "dsc": "Kentucky Derby Mint Julep Gift Set",
-            "price": 59.95
+            "price": 28
         },
         {
             "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
@@ -356,7 +356,7 @@ var MENU = {
             "img": "./assets/img/cardapio/pizzas/choose-your-own-thin-crust-pizza-4-pack.b928a2008eab50c65dc87e59b5952190.jpg",
             "name": "Bartolini's",
             "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
-            "price": 139
+            "price": 55.50
         },
         {
             "id": "choose-your-own-new-haven-style-pizza-6-pack",
@@ -370,21 +370,21 @@ var MENU = {
             "img": "./assets/img/cardapio/pizzas/6-lou-malnatis-deep-dish-pizzas.f59993181da5d295668c8a6fb856055e.jpg",
             "name": "Lou Malnati's Pizza",
             "dsc": "6 Lou Malnati's Deep Dish Pizzas",
-            "price": 116.99
+            "price": 55.50
         },
         {
             "id": "wood-fired-pizzas-best-seller-4-pack",
             "img": "./assets/img/cardapio/pizzas/wood-fired-pizzas-best-seller-4-pack.1653bb05922ba153ac178f8365d27f6d.jpg",
             "name": "Pizzeria Bianco",
             "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
-            "price": 129
+            "price": 55.50
         },
         {
             "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
             "img": "./assets/img/cardapio/pizzas/choose-your-own-deep-dish-pizza-3-pack.4111791511244a4946bb5c9ad2c17da9.jpg",
             "name": "Bartolini's",
             "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
-            "price": 139
+            "price": 52.50
         },
         {
             "id": "choose-your-own-detroit-style-pizza-3-pack",
@@ -405,7 +405,7 @@ var MENU = {
             "img": "./assets/img/cardapio/pizzas/chicago-deep-dish-pizza-4-pack.49927daafa8c147fe9bb2a113e56668e.jpg",
             "name": "My Pi Pizza",
             "dsc": "Chicago Deep Dish Pizza - 4 Pack",
-            "price": 129
+            "price": 55.50
         },
         {
             "id": "4-lou-malnatis-deep-dish-pizzas",
@@ -435,70 +435,70 @@ var MENU = {
             "img": "./assets/img/cardapio/steaks/california-reserve-filet-mignon-steaks-gift-box.bf226e317aad85f47897ae7e325f790d.jpg",
             "name": "Flannery Beef",
             "dsc": "California Reserve Filet Mignon Steaks Gift Box",
-            "price": 129
+            "price": 35
         },
         {
             "id": "steaks-and-cakes-date-night-dinner-for-2",
             "img": "./assets/img/cardapio/steaks/valentines-steaks-and-cakes-dinner-for-2.0c63dab635eed46209455dc33cd25ea8.jpg",
             "name": "Chesapeake Bay Gourmet",
             "dsc": "Steaks and Cakes Date Night Dinner for 2",
-            "price": 129
+            "price": 39
         },
         {
             "id": "Prime-holiday-steak-sampler-for-10-12",
             "img": "./assets/img/cardapio/steaks/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
             "name": "Saltbrick Prime",
             "dsc": "Chef Matt's Steak Sampler for 10-12",
-            "price": 179
+            "price": 36
         },
         {
             "id": "bone-in-rib-steak",
             "img": "./assets/img/cardapio/steaks/bone-in-rib-steak.b13d9d4233035767605f0de9acdce1ab.jpg",
             "name": "Old Homestead Steakhouse",
             "dsc": "Bone-in Rib Steak",
-            "price": 159
+            "price": 35.90
         },
         {
             "id": "american-wagyu-gold-grade-top-sirloins",
             "img": "./assets/img/cardapio/steaks/american-wagyu-gold-grade-sirloins.040dbeb8f8e615b91fa7e513e3dc089c.jpg",
             "name": "Snake River Farms",
             "dsc": "American Wagyu Gold Grade Top Sirloins",
-            "price": 119
+            "price": 35.90
         },
         {
             "id": "2-peter-luger-steak-pack-b",
             "img": "./assets/img/cardapio/steaks/peter-luger-steak-pack-b.9feb0300e6be2dfecfa314f2006a2183.jpg",
             "name": "Peter Luger Steak House",
             "dsc": "Peter Luger Porterhouse Steaks",
-            "price": 215.95
+            "price": 35.95
         },
         {
             "id": "ribeye-prime-steak-gift-box",
             "img": "./assets/img/cardapio/steaks/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
             "name": "Churchill's Steakhouse",
             "dsc": "Ribeye Prime Steak Gift Box",
-            "price": 229
+            "price": 35.90
         },
         {
             "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
             "img": "./assets/img/cardapio/steaks/usda-prime-black-angus-filet-mignon-barrel-cut.6ee213799e7d1848763d12edca18e3b1.jpg",
             "name": "Pat LaFrieda Meats",
             "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
-            "price": 96.7
+            "price": 35.70
         },
         {
             "id": "california-reserve-ribeye-steak",
             "img": "./assets/img/cardapio/steaks/california-reserve-ribeye-steak-12-oz.d367c5ae72dd9f89e170662104bef4fc.jpg",
             "name": "Flannery Beef",
             "dsc": "California Reserve Ribeye Steak - 12 oz",
-            "price": 32
+            "price": 35.90
         },
         {
             "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
             "img": "./assets/img/cardapio/steaks/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
             "name": "Chef Francis Mallmann",
             "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
-            "price": 225
+            "price": 35.90
         },
         {
             "id": "california-reserve-filet-mignon-steak",
@@ -512,7 +512,7 @@ var MENU = {
             "img": "./assets/img/cardapio/steaks/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
             "name": "Perini Ranch Steakhouse",
             "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
-            "price": 165
+            "price": 35.90
         }
     ]
 }
